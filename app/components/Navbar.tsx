20fbd714ba074c4dfd8756ae7dc42d0e2fc29bff
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-03 01:02:06 +0530

    modified_navbar: Modifed the navbar element to show for selected pages

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,18 @@
 'use client'
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
   const [isPressedButton1, setIsPressedButton1] = useState(false);
   const [isPressedButton2, setIsPressedButton2] = useState(false);
   const [isPressedButton3, setIsPressedButton3] = useState(false);
+  const currentPath = usePathname(); 
+  const urlParams = new URLSearchParams(window.location.search);
+  const avatar_url = currentPath === '/questions' ? urlParams.get('avatar_url') : null;
 
   return (
-    <nav className='bg-[#131313] w-[100vw] h-[7vh] fixed flex justify-between items-center'>
+    <nav className='bg-[#131313] w-[100vw] h-[7vh] fixed flex justify-between items-center z-10'>
         <div className='bg-[#1c1c1e] w-[20%] h-[80%] rounded-t-xl flex items-center justify-center ml-8'>  
             <div className='relative w-[50%] h-[50%] flex justify-center items-center gap-4'>
                 <div className='relative w-full h-full flex justify-center items-center'>
@@ -36,18 +40,26 @@ const Navbar = () => {
             </div>
         </div>
 
-        <div className='bg-[#1c1c1e] w-[10%] h-[80%] rounded-t-xl flex items-center justify-center mr-8'>  
-            <div className='relative w-[60%] h-[50%] flex justify-center items-center'>
-                <div className='absolute bg-black w-full h-full rounded-lg top-[0.60rem] left-0 z-0'></div>
-                <button 
-                    className={`relative bg-[#454545] w-full h-full flex items-center justify-center p-4 rounded-lg text-zinc-100 font-bold gap-2 z-10 ${isPressedButton3 ? 'bulge' : ''}`}
-                    onMouseDown={() => setIsPressedButton3(true)}
-                    onMouseUp={() => setIsPressedButton3(false)}
-                >
-                    Sign In
-                </button>
-            </div>
-        </div>
+        {currentPath !== '/Signin' && currentPath !== '/questions' && (
+          <div className='bg-[#1c1c1e] w-[10%] h-[80%] rounded-t-xl flex items-center justify-center mr-8'>  
+              <div className='relative w-[60%] h-[50%] flex justify-center items-center'>
+                  <div className='absolute bg-black w-full h-full rounded-lg top-[0.60rem] left-0 z-0'></div>
+                  <button 
+                      className={`relative bg-[#454545] w-full h-full flex items-center justify-center p-4 rounded-lg text-zinc-100 font-bold gap-2 z-10 ${isPressedButton3 ? 'bulge' : ''}`}
+                      onMouseDown={() => setIsPressedButton3(true)}
+                      onMouseUp={() => setIsPressedButton3(false)}
+                  >
+                      Sign In
+                  </button>
+              </div>
+          </div>
+        )}
+
+        {currentPath === '/questions' && avatar_url && (
+          <div className='flex items-center justify-center mr-8'>
+            <Image src={avatar_url} alt='Avatar' height={40} width={40} className='rounded-full' />
+          </div>
+        )}
     </nav>
   )
 }
